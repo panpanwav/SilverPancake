c872d568dd726652eb40bd2f933cbd77ba4678c6
panpanwav <wendtfe10@gmail.com>
2025-05-30 16:06:30 -0300

    Update Problem.cpp

--- codeplay/1132 beecrowd/Problem.cpp
+++ codeplay/1132 beecrowd/Problem.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 using namespace std;
 
 class Problema {
@@ -24,5 +25,43 @@ class Problema {
     };
     int getResult(){
         return result;
+    };
+    void leitura(){
+        int *placeholder;
+        placeholder = new int;
+        if (!placeholder) {
+        cout << "allocation of memory failed";
+        exit(0);
+    }
+        cin >> *placeholder;
+        setVal1(*placeholder);
+        cin >> *placeholder;
+        setVal2(*placeholder);
+        if (getVal2() < getVal1()){
+            *placeholder = 0;
+            int placeholder = getVal2();
+            setVal2(getVal1());
+            setVal1(placeholder);
+    }
+        
+        delete placeholder;
+    };
+    int calculo(){
+        int *resultado;
+        resultado = new int;
+        *resultado = 0;
+        if (!resultado) {
+            cout << "allocation of memory failed";
+            exit(0);
+        }
+        for (int i = getVal1() ; i <= getVal2(); i++){
+            if(i%13 != 0){
+               *resultado += i;
+            }
+        }
+        setResult(*resultado);
+        delete resultado;
+        
+        return getResult();
     };
 };
