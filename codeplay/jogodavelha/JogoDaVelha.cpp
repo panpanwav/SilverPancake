5c4e710b4e286f2419a7c3cfa0d9759931a7e3e8
panpanwav <wendtfe10@gmail.com>
2025-06-02 18:54:47 -0300

    Update JogoDaVelha.cpp

--- codeplay/jogodavelha/JogoDaVelha.cpp
+++ codeplay/jogodavelha/JogoDaVelha.cpp
@@ -1,60 +1,82 @@
 #include <iostream>
 #include <stdio.h>
 #include <string>
+#include "Jogador.cpp"
+#include "JogoDaVelha.cpp"
 using namespace std;
 
-class JogoDaVelha{
-    private:
-        char tabuleiro[3][3];
-    public:
-        void setTabuleiro(int arr1, int arr2, char read){
-            tabuleiro[arr1][arr2] = read;
-        }
-        char getCellTabuleiro(int arr1, int arr2){
-            return tabuleiro[arr1][arr2];
-        }
-        void zerarTabuleiro(){
-            for(int i = 0; i < 3; i++){
-                for (int j = 0; j < 3; j++){
-                    tabuleiro[i][j] = '+';
+int main()
+{
+    Jogador player[2];
+    JogoDaVelha game;
+    string lerString;
+    bool liberar;
+    bool finalFase;
+    bool finalJogo = false;
+    int rodada;
+    int partidaAtual = 0;
+    int jogadorDaRodada;
+    int val1, val2;
+    
+    cout << "Jogador 1: Insira seu nome:\n";
+    cin >> lerString;
+    player[0].setNomeJogador(lerString);
+    cout << "Jogador 2: Insira seu nome:\n";
+    cin >> lerString;
+    player[1].setNomeJogador(lerString);
+    player[0].setInsert('x');
+    player[1].setInsert('o');
+    game.zerarTabuleiro();
+    
+    while(finalJogo == false){
+        partidaAtual++;
+        cout << "Partida " << partidaAtual << "\n";
+        finalFase = false;
+        rodada = -1;
+        while (finalFase == 0){
+            rodada++;
+            if(rodada%2 == 0){
+                jogadorDaRodada = 0;
+            }
+            else{
+                jogadorDaRodada = 1;
+            }
+            liberar = false;
+            while (liberar == false){
+                cout << player[jogadorDaRodada].getNomeJogador() << ", insira o valor da linha e da coluna (de 1 a 3)\n";
+                game.displayTabuleiro();
+                cin >> val1 >> val2;
+                val1--;
+                val2--;
+                liberar = game.verificarVazio(val1,val2);
+                if (liberar == true){
+                    game.setTabuleiro(val1,val2,player[jogadorDaRodada].getInsert());
+                }
+                else {
+                    cout << "O bloco está ocupado com o valor " << game.getCellTabuleiro(val1,val2) << "\n";
                 }
             }
-        }
-        void displayTabuleiro(){
-            for(int i = 0; i < 3; i++){
-                for(int j = 0; j < 3; j++){
-                    cout << tabuleiro[i][j];
-                    if (j == 2){
-                        cout << "\n";
-                    }
-                }
+            finalFase = game.verificarFim(player[jogadorDaRodada].getInsert());
+            if (finalFase == true){
+                game.displayTabuleiro();
+                player[jogadorDaRodada].setVitorias(+1);
+                cout << "O vencedor é " << player[jogadorDaRodada].getNomeJogador() << "\n";
+                game.displayTabuleiro();
+                cout << "Deseja continuar? Insira: \n 1 - sim \n 2 - não\n";
             }
         }
-        bool verificarVazio(int arr1, int arr2){
-            if (tabuleiro[arr1][arr2] == '+'){
-                return true;
-            }
-            else {
-                return false;
-            }
+        cin >> jogadorDaRodada;
+        while (jogadorDaRodada != 1 && jogadorDaRodada != 2){
+            cout << "valor inválido, tente novamente!\n";
+            cin >> jogadorDaRodada;
         }
-        bool verificarFim(char read){
-            bool holder = false;
-            for (int i = 0; i < 3; i++){
-                if (read == tabuleiro[i][0] && read == tabuleiro[i][1] && read == tabuleiro[i][2]){
-                    holder = true;
-                }
-                    
-                if (read == tabuleiro[0][i] && read == tabuleiro[1][i] && read == tabuleiro[2][i]){
-                    holder = true;
-                }
-            }
-            if (read == tabuleiro[0][0] && read == tabuleiro[1][1] && read == tabuleiro[2][2]){
-                holder = true;
-            }
-            if (read == tabuleiro[2][0] && read == tabuleiro[1][1] && read == tabuleiro[0][2]){
-                holder = true;
-            }
-            return holder;
+        if (jogadorDaRodada == 1){
+            finalJogo = true;
         }
-};
+        else {
+            finalJogo == false;
+        }
+    }
+    
+    return 0;
+}
