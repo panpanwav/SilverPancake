f442ad7f837ff373d8e37408087ff6fef7495c25
panpanwav <wendtfe10@gmail.com>
2025-06-02 16:52:03 -0300

    Update main.cpp

--- codeplay/jogodavelha/main.cpp
+++ codeplay/jogodavelha/main.cpp
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include "Jogador.cpp"
+#include "JogoDaVelha.cpp"
+using namespace std;
 
+int main()
+{
+    Jogador player1;
+    Jogador player2;
+    JogoDaVelha game;
+    string lerString;
+    
+    cout << "Jogador 1: Insira seu nome:\n";
+    cin >> lerString;
+    player1.setNomeJogador(lerString);
+    cout << "Jogador 2: Insira seu nome:\n";
+    cin >> lerString;
+    player2.setNomeJogador(lerString);
+    player1.setInsert = x;
+    player2.setInsert = o;
+    JogoDaVelha.zerarTabuleiro();
+
+    return 0;
+}
