c91e12560099e65ae299449ea52b9dbbc6623308
panpanwav <wendtfe10@gmail.com>
2025-06-02 16:32:31 -0300

    Create Jogador.cpp

---
+++ codeplay/jogodavelha/Jogador.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include "JogoDaVelha.cpp"
+using namespace std;
+
+class Jogador{
+    private:
+        string nomeJogador;
+        char insert;
+        int vitorias = 0;
+    
+    public:
+        string getNomeJogador(){
+            return nomeJogador;
+        }
+        char getInsert(){
+            return insert;
+        }
+        int getVitorias(){
+            return vitorias;
+        }
+        void setNomeJogador(string read){
+            nomeJogador = read;
+        }
+        void setInsert(char read){
+            nomeJogador = read;
+        }
+        void setVitorias(int read){
+            vitorias = read;
+        }
+};
