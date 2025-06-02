e8f8c01c71a1fc1564a11753e5507d144782ce43
panpanwav <wendtfe10@gmail.com>
2025-06-02 17:34:08 -0300

    Update main.cpp

--- codeplay/jogodavelha/main.cpp
+++ codeplay/jogodavelha/main.cpp
@@ -7,20 +7,39 @@ using namespace std;
 
 int main()
 {
-    Jogador player1;
-    Jogador player2;
+    Jogador player[2];
     JogoDaVelha game;
     string lerString;
+    bool finalFase = false;
+    bool finalJogo = false;
+    int rodada;
+    int jogadorDaRodada;
+    int val1, val2;
     
     cout << "Jogador 1: Insira seu nome:\n";
     cin >> lerString;
-    player1.setNomeJogador(lerString);
+    player[0].setNomeJogador(lerString);
     cout << "Jogador 2: Insira seu nome:\n";
     cin >> lerString;
-    player2.setNomeJogador(lerString);
-    player1.setInsert = x;
-    player2.setInsert = o;
-    JogoDaVelha.zerarTabuleiro();
-
+    player[1].setNomeJogador(lerString);
+    player[0].setInsert('x');
+    player[1].setInsert('o');
+    game.zerarTabuleiro();
+    while(finalJogo == false){
+        rodada = 0;
+        while (finalFase == false){
+            if(rodada%2 == 0){
+                jogadorDaRodada = 0;
+            }
+            else{
+                jogadorDaRodada = 1;
+            }
+        game.displayTabuleiro();
+            cout << player[jogadorDaRodada].getNomeJogador() << " , insira o valor da linha e da coluna (de 1 a 3)\n";
+            cin >> val1 >> val2;
+            
+            rodada++;
+        }
+    }
     return 0;
 }
